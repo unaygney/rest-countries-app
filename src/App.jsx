4a2a1d44c1dfee6225d85a0d0ccc0d0a46203f69
Guney Unay <unaygney17@gmail.com>
2023-09-03 12:29:22 +0300

    added context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from "react"
-import "./main.css"
+import React from "react";
+import { useContext } from "react";
+import "./main.css";
+import ThemeContext from "./context/ThemeContext";
 
 function App() {
- 
+  const { theme, setTheme } = useContext(ThemeContext);
+  console.log(theme);
 
+  
   return (
     <>
-test
+      <button onClick={() => (theme ? setTheme(false) : setTheme(true))}>
+        Buton
+      </button>
+      <p>{theme ? "True" : "False"}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
