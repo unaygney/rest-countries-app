c65724cd036976d7bac874d9e3214668fe21122e
Guney Unay <guneymarketing17@hotmail.com>
2023-09-03 14:55:19 +0300

    minor design update.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { useContext } from "react";
 import "./main.css";
-import ThemeContext from "./context/ThemeContext";
+
 
 //Layout
 import Navbar from "./components/layout/Navbar";
 //Components
 import SearchBar from "./components/SearchBar";
 function App() {
-  const { theme, setTheme } = useContext(ThemeContext);
+ 
 
 
   
